adb39d1f06655cd4e80f052857c4aa8717fdda95
LBilev <19113@uktc-bg.com>
2023-03-30 21:47:25 +0300

    fixed folders a bit

--- Pages/SignIN.php
+++ Pages/SignIN.php
@@ -1,6 +1,6 @@
 <?php 
 
-include 'dbconnect.php';
+include '../Db/dbconnect.php';
 
 if ( isset( $_POST['submit'] ) ) {
 
@@ -15,7 +15,7 @@ if ( isset( $_POST['submit'] ) ) {
 	$stmt->execute([ $email, $hash ]); 
 	$user = $stmt->fetch();
 
-    include 'logInValidation.php';
+    include '../Validation/logInValidation.php';
 	
 	if ( $user ) {
 			
@@ -40,7 +40,7 @@ if ( isset( $_POST['submit'] ) ) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="SignUPcssDark.css">
+    <link rel="stylesheet" href="../Css/SignUPcssDark.css">
     <title>Sign In</title>
 </head>
 <body>
